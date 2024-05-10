d4428fea226b95f073ad709d3c3b26169c418b70
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-05-11 00:44:09 +0600

    body

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,11 +7,35 @@ const Header = () => {
         <div className="  bg-slate-900 p-6">
             <nav>
                 <ul className="text-3xl  text-center space-x-10 font-extrabold ">
-                    <NavLink to="/home">Home</NavLink>
-                    <NavLink to="/items">Items</NavLink>
+                    <NavLink  className={({ isActive, isPending }) =>
+                      isActive
+                        ? "underline bg-fuchsia-500 p-2 rounded-br-3xl rounded-tl-3xl text-white"
+                        : isPending
+                        ? "  opacity-25"
+                        : ""
+                    } to="/home">Home</NavLink>
+                    <NavLink  className={({ isActive, isPending }) =>
+                      isActive
+                        ? "underline bg-fuchsia-500 p-2 rounded-br-3xl rounded-tl-3xl text-white"
+                        : isPending
+                        ? "  opacity-25"
+                        : ""
+                    } to="/items">Items</NavLink>
      
-                    <NavLink to="/contact">Contact Us</NavLink>
-                    <NavLink to="/about">About Us</NavLink>
+                    <NavLink  className={({ isActive, isPending }) =>
+                      isActive
+                        ? "bg-fuchsia-500 p-2 rounded-br-3xl rounded-tl-3xl text-white"
+                        : isPending
+                        ? "  opacity-25"
+                        : ""
+                    } to="/contact">Contact Us</NavLink>
+                    <NavLink  className={({ isActive, isPending }) =>
+                      isActive
+                        ? "bg-fuchsia-500 p-2 rounded-br-3xl rounded-tl-3xl text-white"
+                        : isPending
+                        ? "  opacity-25"
+                        : ""
+                    } to="/about">About Us</NavLink>
                 </ul>
             </nav>
         </div>
