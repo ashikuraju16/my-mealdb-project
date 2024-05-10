f89ecc30951becee2987900aed3c0bcc144232f4
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-05-10 10:22:36 +0600

    home

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Home from "./Components/Home/Home.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>,
+    element: <Home />,
     children: [{}],
   },
 ]);
